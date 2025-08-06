680f80c50fa7878923bcd5e1609d25a58c5aa09e
s-nagar53 <saginaw5253@gmail.com>
2025-08-06 15:07:33 -0500

    deleting extra files

--- backend/data/memoryMatches.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  matches: [],
-  matchIdCounter: 1,
-  
-  // Helper function to get matches by event ID
-  getMatchesByEventId: function(eventId) {
-    return this.matches.filter(match => match.eventId === eventId);
-  }
-};
